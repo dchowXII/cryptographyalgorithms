b0f6501e5fb734e56f239694026ab7a6d7ad7d64
Dylan C <chowd9@students.rowan.edu>
2020-01-29 22:40:27 -0500

    Started to work on triple des

---
+++ tripledes.py
@@ -0,0 +1,14 @@
+# Attempt at Triple DES Alogrithm
+# author Dylan Chow
+# started on 1/29/2020
+
+# step 1: create 3 independent keys.
+# drawkback: man in middle attack vulnerable
+def keys_create():
+    key1 
+
+# step 2: des encrypt with k1
+
+# step 3: des decrypt with k2
+
+# step 4: des encrypt with k3
